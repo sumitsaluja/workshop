c818086d13dc4edeb8ec7c7f57a8f68db18fc1b8
Sumit <sumitaluja@ymail.com>
2024-10-28 22:21:14 -0400

    Fixed module load

--- cpu_jobs/R/job.sh
+++ cpu_jobs/R/job.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 #SBATCH --job-name=mean_vectors      # Job name
 #SBATCH --output=mean_vectors.out     # Standard output and error log
 #SBATCH --ntasks=1                    # Run on a single task
@@ -6,8 +6,8 @@
 #SBATCH --time=00:05:00               # Time limit hrs:min:sec
 #SBATCH --mem=4G                      # Memory limit
 
-# Load the R module 
-module load load R/4.2.1
+# Load the R module
+module load R/4.2.1
 
 # Run the R script
 Rscript mean_vectors.R
